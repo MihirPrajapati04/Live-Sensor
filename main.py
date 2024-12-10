b906885c4033101f2f0f4682b2ddb2440f4aa0d5
MihirPrajapati04 <prajapatimihir685@gmail.com>
2024-12-10 21:45:59 +0530

    exception and logger files added

---
+++ main.py
@@ -0,0 +1,17 @@
+import os 
+import sys
+from sensor.exception import SensorException
+
+
+def test_exception():
+    try:
+        a=1/0
+    except Exception as e:
+       raise SensorException(e,sys) 
+    
+
+if __name__=="__main__":
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
