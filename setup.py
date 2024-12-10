97b0e251ff33af8e589f521329c604e862373e6f
MihirPrajapati04 <prajapatimihir685@gmail.com>
2024-12-10 18:32:11 +0530

    get_requirements() updated

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from typing import List
 
 
 
-def get_requirements()-> list[str]:
+def get_requirements()-> List[str]:
 
-    requirements_list : list[str] = []
+    requirements_list : List[str] = []
 
     return requirements_list
 
