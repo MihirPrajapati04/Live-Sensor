fec5d125bd866c103b364265771b1ba0722341e7
MihirPrajapati04 <prajapatimihir685@gmail.com>
2024-12-13 23:52:58 +0530

    files added

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import os
 import sys
 from sensor.exception import SensorException
 from sensor.logger import logging
+from sensor.utils import dump_csv_file_to_mongodb_collection
 
 
-def test_exception():
-    try:
-        a=1/0
-    except Exception as e:
-       raise SensorException(e,sys) 
+# def test_exception():
+#     try:
+#         a=1/0
+#     except Exception as e:
+#        raise SensorException(e,sys) 
     
 
 if __name__=="__main__":
-    try:
-        logging.info("error will be here")
-        test_exception()
-    except Exception as e:
-        print(e)
+    file_path = "C:/Users/praja\Live-Sensor/aps_failure_training_set1.csv"
+    database = "MyDataBase"
+    collection = "Sensor"
+    dump_csv_file_to_mongodb_collection(file_path=file_path,database_name=database,collection_name=collection)
+    
+    
